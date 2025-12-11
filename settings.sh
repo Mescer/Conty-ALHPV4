acb6c526b0316ad129d584b89c8377aae7799cf0
Mescer <mescer@gmail.com>
2025-12-11 06:14:28 +0300

    Update settings.sh

--- settings.sh
+++ settings.sh
@@ -1,4 +1,3 @@
-
 # shellcheck shell=bash disable=2034
 
 # Packages to install
@@ -6,18 +5,23 @@
 # Apart from packages from the official Arch repos, you can also specify
 # packages from the Chaotic-AUR repo
 PACKAGES=(
+
 	# audio
 	alsa-lib lib32-alsa-lib alsa-plugins lib32-alsa-plugins libpulse
 	lib32-libpulse alsa-tools alsa-utils pipewire lib32-pipewire pipewire-pulse pipewire-jack lib32-pipewire-jack
+
 	# core
 	xorg-xwayland qt6-wayland wayland lib32-wayland qt5-wayland xorg-server-xephyr gamescope
+
 	# video
 	mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon
 	vulkan-intel lib32-vulkan-intel
 	vulkan-icd-loader lib32-vulkan-icd-loader vulkan-mesa-layers
 	lib32-vulkan-mesa-layers libva-intel-driver lib32-libva-intel-driver
 	intel-media-driver mesa-utils vulkan-tools libva-utils lib32-mesa-utils
+
 	# wine
+        # winetricks-git 
 	wine-staging winetricks wine-nine wineasio
 	freetype2 lib32-freetype2 libxft lib32-libxft
 	flex lib32-flex fluidsynth lib32-fluidsynth
@@ -28,7 +32,7 @@ PACKAGES=(
 	krb5 lib32-krb5 libpulse lib32-libpulse alsa-plugins
 	lib32-alsa-plugins alsa-lib lib32-alsa-lib gnutls lib32-gnutls
 	giflib lib32-giflib gst-libav gst-plugin-pipewire gst-plugins-ugly
-	gst-plugins-bad gst-plugins-bad-libs
+	gst-plugins-bad gst-plugins-bad-libs gst-plugin-va lsp-plugins-gst
 	gst-plugins-base-libs lib32-gst-plugins-base-libs gst-plugins-base lib32-gst-plugins-base
 	gst-plugins-good lib32-gst-plugins-good gstreamer lib32-gstreamer
 	libpng lib32-libpng v4l-utils lib32-v4l-utils
@@ -38,12 +42,24 @@ PACKAGES=(
 	gtk3 lib32-gtk3 vulkan-icd-loader lib32-vulkan-icd-loader
 	sdl2-compat lib32-sdl2-compat vkd3d lib32-vkd3d libgphoto2
 	openssl-1.1 lib32-openssl-1.1 libnm lib32-libnm
-	cabextract wget mangohud lib32-mangohud
+	cabextract wget gamemode lib32-gamemode mangohud lib32-mangohud
+        goverlay
+
 	# development
 	# base-devel git meson mingw-w64-gcc cmake
+
 	# gaming
-	lutris python-protobuf steam bottles
+        lutris python-protobuf steam bottles
+	# lutris python-protobuf steam steamtinkerlaunch
+	# minigalaxy gamehub legendary prismlauncher bottles playonlinux obs-studio
+	# retroarch retroarch-assets-ozone libretro-beetle-psx-hw
+	# libretro-blastem libretro-bsnes libretro-dolphin duckstation-gpl
+	# libretro-gambatte libretro-melonds libretro-mgba libretro-nestopia
+	# libretro-parallel-n64 libretro-picodrive libretro-ppsspp
+	# libretro-yabause pcsx2-git lsfg-vk-git
+
 	# extra
+        gnome-themes-extra
 	# nano ttf-dejavu ttf-liberation firefox mpv geany pcmanfm
 	# htop qbittorrent speedcrunch gpicview file-roller openbox lxterminal
 	# yt-dlp minizip nautilus genymotion jre17-openjdk gnome-themes-extra
@@ -51,7 +67,7 @@ PACKAGES=(
 )
 
 # If you want to install AUR packages, specify them in this variable
-AUR_PACKAGES=(yay-bin)
+AUR_PACKAGES=(yay-bin mangojuice-bin faugus-launcher-git)
 
 # ALHP is a repository containing packages from the official Arch Linux
 # repos recompiled with -O3, LTO and optimizations for modern CPUs for
@@ -70,15 +86,13 @@ ALHP_FEATURE_LEVEL=4
 
 # Locales to configure in the image
 LOCALES=(
-        'en_US.UTF-8 UTF-8'
-        'ru_RU.UTF-8 UTF-8'
+	'en_US.UTF-8 UTF-8'
+	'ru_RU.UTF-8 UTF-8'
 )
 
 # Content of pacman mirrorrlist file before reflector is installed and used to fetch new one
 # shellcheck disable=2016
 MIRRORLIST='
-# Server = https://mirror.cspacehostings.com/archlinux/$repo/os/$arch
-# Server = https://berlin.mirror.pkgbuild.com/$repo/os/$arch
 Server = https://de.arch.mirror.kescher.at/$repo/os/$arch
 Server = https://europe.mirror.pkgbuild.com/$repo/os/$arch
 Server = https://at.arch.mirror.kescher.at/$repo/os/$arch
@@ -88,6 +102,16 @@ Server = https://mirror.telepoint.bg/archlinux/$repo/os/$arch
 Server = https://mirror.pseudoform.org/$repo/os/$arch
 Server = https://archlinux.thaller.ws/$repo/os/$arch
 '
+#Server = https://mirror1.sl-chat.ru/archlinux/$repo/os/$arch
+#Server = https://mirror3.sl-chat.ru/archlinux/$repo/os/$arch
+#Server = https://us.mirrors.cicku.me/archlinux/$repo/os/$arch
+#Server = https://mirror.osbeck.com/archlinux/$repo/os/$arch
+#Server = https://md.mirrors.hacktegic.com/archlinux/$repo/os/$arch
+#Server = https://geo.mirror.pkgbuild.com/$repo/os/$arch
+#Server = https://mirror.qctronics.com/archlinux/$repo/os/$arch
+#Server = https://arch.mirror.constant.com/$repo/os/$arch
+#Server = https://america.mirror.pkgbuild.com/$repo/os/$arch
+#Server = https://mirror.tmmworkshop.com/archlinux/$repo/os/$arch
 
 # Enable this variable to use the system-wide mksquashfs/mkdwarfs instead
 # of those provided by the Conty project
@@ -105,23 +129,23 @@ SQUASHFS_COMPRESSOR_ARGUMENTS=(-b 1M -comp "${SQUASHFS_COMPRESSOR}" -Xcompressio
 #SQUASHFS_COMPRESSOR_ARGUMENTS=(-b 256K -comp "${SQUASHFS_COMPRESSOR}" -Xhc)
 
 # Set to any value to Use DwarFS instead of SquashFS
-#USE_DWARFS=
-#DWARFS_COMPRESSOR_ARGUMENTS=(
-#	-l7 -C zstd:level=19 --metadata-compression null
-#	-S 22 -B 1 --order nilsimsa
-#	-W 12 -w 4 --no-history-timestamps --no-create-timestamp
-#)
+USE_DWARFS=
+DWARFS_COMPRESSOR_ARGUMENTS=(
+	-l7 -C zstd:level=19 --metadata-compression null
+	-S 22 -B 1 --order nilsimsa
+	-W 12 -w 4 --no-history-timestamps --no-create-timestamp
+)
 
 
 # List of links to arch bootstrap archive
 # Conty will try to download each one of them sequentially
 BOOTSTRAP_DOWNLOAD_URLS=(
 	'https://arch.hu.fo/archlinux/iso/latest/archlinux-bootstrap-x86_64.tar.zst'
-	#'https://mirror.cyberbits.eu/archlinux/iso/latest/archlinux-bootstrap-x86_64.tar.zst'
-	#'https://mirror.osbeck.com/archlinux/iso/latest/archlinux-bootstrap-x86_64.tar.zst'
-	#'https://mirror.lcarilla.de/archlinux/iso/latest/archlinux-bootstrap-x86_64.tar.zst'
-	#'https://mirror.moson.org/archlinux/iso/latest/archlinux-bootstrap-x86_64.tar.zst'
-	#'https://mirror.f4st.host/archlinux/iso/latest/archlinux-bootstrap-x86_64.tar'
+	'https://mirror.cyberbits.eu/archlinux/iso/latest/archlinux-bootstrap-x86_64.tar.zst'
+	'https://mirror.osbeck.com/archlinux/iso/latest/archlinux-bootstrap-x86_64.tar.zst'
+	'https://mirror.lcarilla.de/archlinux/iso/latest/archlinux-bootstrap-x86_64.tar.zst'
+	'https://mirror.moson.org/archlinux/iso/latest/archlinux-bootstrap-x86_64.tar.zst'
+	'https://mirror.f4st.host/archlinux/iso/latest/archlinux-bootstrap-x86_64.tar.zst'
 )
 
 # sha256sums.txt file to verify downloaded bootstrap archive with
